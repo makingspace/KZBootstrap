d8d10f26923df119a95b708618c4b40ffa47fda7
altyus <alctyus@gmail.com>
2015-11-12 11:21:38 -0500

    modifying scripts

--- Pod/Assets/Scripts/bootstrap.sh
+++ Pod/Assets/Scripts/bootstrap.sh
@@ -1,35 +1,24 @@
 #!/bin/sh
 if [ "$#" -eq 0 ] ; then
-    ENABLE_LINES=1
     ENABLE_TODO=1
     ENABLE_PERUSER=1
-    ENABLE_BUILDNBR=1
     ENABLE_ICONVERSIONING=1
 else
-    ENABLE_LINES=0
     ENABLE_TODO=0
     ENABLE_PERUSER=0
-    ENABLE_BUILDNBR=0
     ENABLE_ICONVERSIONING=0
 
     for key in "$@"
     do
     	case $key in
-		-l)
-		    ENABLE_LINES=1
-		    ;;
 		-t)
 		    ENABLE_TODO=1
 		    ;;
 		-u)
 		    ENABLE_PERUSER=1
 		    ;;
-		-n)
-		    ENABLE_BUILDNBR=1
-		    ;;
 		-i)
 		    ENABLE_ICONVERSIONING=1
-		    ENABLE_BUILDNBR=1
 		    ;;
 		*)
 	            # unknown option
@@ -43,16 +32,8 @@ fi
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${DIR}" ]]; then DIR="${PWD}"; fi
 
-if [ "$ENABLE_BUILDNBR" -eq 1 ] ; then
-    "${DIR}/buildNumber.sh"
-fi
-
 if [ "$ENABLE_ICONVERSIONING" -eq 1 ] ; then
     "${DIR}/iconVersioning.sh"
-fi
-
-if [ "$ENABLE_LINES" -eq 1 ] ; then
-    "${DIR}/lines.sh"
 fi
 
 if [ "$ENABLE_TODO" -eq 1 ] ; then
